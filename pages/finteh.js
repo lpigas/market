f1232bd89cc9ebbdd5a557ba1c93ea88f16b0241
lpigas <igornixdn@gmail.com>
2022-08-08 21:45:19 +0300

    end balace part

--- pages/finteh.js
+++ pages/finteh.js
@@ -1,12 +1,39 @@
 import React from "react";
-import FullReportsBlock from "../scenes/finteh/Fullreportsblock";
+
 import LayoutFinteh from "../components/layout/finteh/LayoutFinteh";
+import { useRouter } from "next/router";
+import StandButtons from "../components/atoms/Buttons/standart/StandButtons";
 
 export default function finteh() {
+  const router = useRouter()
   return (
     <LayoutFinteh>
-      <div className="h-screen flex items-center justify-center">
-        <FullReportsBlock />
+      <div className="h-screen flex items-center justify-center flex-col">
+        <div className=" bg-cyan-500 flex flex-col h-96 item-center justify-center px-14 rounded-2xl">
+          <div className="my-2">
+      <StandButtons size="xxl" value="Reports from orders" onClick={()=> router.push('/finteh/reportorders')} />
+          </div>
+          <div className="my-2">
+      <StandButtons
+        size="xxl"
+        value="Change balances"
+        onClick={() => router.push("/finteh/balance")}
+      />
+      </div>
+      <div className="my-2">
+      <StandButtons size="xxl" value="Reports from cash" />
+      </div>
+      <div className="my-2">
+      <StandButtons
+        size="xxl"
+        value="Add new product"
+        onClick={() => router.push("/finteh/newproduct")}
+      />
+      </div>
+      <div className="my-2">
+      <StandButtons size="xxl" value="Add new group" />
+      </div>
+      </div>
       </div>
     </LayoutFinteh>
   );
